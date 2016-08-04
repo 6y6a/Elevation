e0cbdb4d895b5067221ee0b66f546309b730c979
6y6a <gubinalexeymain@gmail.com>
2016-08-04 18:59:49 +0300

    Add animation

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+zoom = new WOW(
+    {
+        boxClass:     'animation',      // default
+        animateClass: 'animated zoomIn', // default
+        offset:       300,          // default
+        mobile:       true,       // default
+        live:         true        // default
+    }
+);
+
+zoom.init();
